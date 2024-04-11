05396f68d2e77be44ab6c531f40dda74fab33d17
Luis Alberto Rodrigo <a_luisal@hotmail.com>
2024-04-11 18:35:10 +0200

    Added Stories from lloydrichards/shadcn-ui repo feat/storybook-cli

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,25 +1,41 @@
-import type { Preview } from "@storybook/react";
+import { Preview, ReactRenderer } from '@storybook/react';
+import '../src/globals.css';
 import { withThemeByClassName } from '@storybook/addon-themes';
-import "../src/globals.css"
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    decorators: [
+       withThemeByClassName<ReactRenderer>({
+         themes: {
+           light: '',
+           dark: 'dark',
+         },
+         defaultTheme: 'light',
+       }),
+      ]
   },
-  decorators: [
-    withThemeByClassName({
-      themes: {
-        light: 'light',
-        dark: 'dark',
-      },
-      defaultTheme: 'light',
-    }),
-  ]
 };
 
 export default preview;
+
+// import { withThemeByDataAttribute } from '@storybook/addon-themes';
+
+// export const decorators = [
+//   withThemeByDataAttribute({
+//     themes: {
+//       light: 'light',
+//       dark: 'dark',
+//     },
+//     defaultTheme: 'light',
+//     attributeName: 'data-mode',
+//   }),
+// ];
+
+// export default preview;
